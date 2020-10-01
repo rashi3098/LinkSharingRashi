643bbdd9eca85fdc073dcc2699ef9dbccf19bd3c
rashi3098 <72015503+rashi3098@users.noreply.github.com>
2020-10-01 14:28:21 +0530

    Add files via upload

---
+++ src/test/groovy/linksharing3/JQueryControllerSpec.groovy
@@ -0,0 +1,18 @@
+package linksharing3
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class JQueryControllerSpec extends Specification implements ControllerUnitTest<JQueryController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
